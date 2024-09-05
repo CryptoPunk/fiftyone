df69d1536fe38f6d988c21082a6cc644b1d464d8
imanjra <ibrahim@voxel51.com>
2024-09-05 09:07:29 -0400

    fixes and enhancements for modal spaces

--- app/packages/core/src/components/Modal/Modal.tsx
+++ app/packages/core/src/components/Modal/Modal.tsx
@@ -9,7 +9,6 @@ import { ModalActionsRow } from "../Actions";
 import Sidebar from "../Sidebar";
 import { useLookerHelpers } from "./hooks";
 import { modalContext } from "./modal-context";
-import ModalNavigation from "./ModalNavigation";
 import { ModalSpace } from "./ModalSpace";
 import { TooltipInfo } from "./TooltipInfo";
 import { useModalSidebarRenderEntry } from "./use-sidebar-render-entry";
@@ -38,26 +37,13 @@ const ModalContainer = styled.div`
   z-index: 10001;
 `;
 
-const ModalNavigationContainer = styled.div<{
-  $sidebarWidth: number;
-  $isSidebarVisible: boolean;
-}>`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: ${({ $sidebarWidth, $isSidebarVisible }) =>
-    $isSidebarVisible ? `calc(100% - ${$sidebarWidth}px)` : "100%"};
-  height: 100%;
-  position: absolute;
-  left: 0;
-`;
-
 const SpacesContainer = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   overflow: hidden;
+  z-index: 1501;
 `;
 
 const SidebarPanelBlendInDiv = styled.div`
@@ -91,10 +77,7 @@ const Modal = () => {
 
   const renderEntry = useModalSidebarRenderEntry();
 
-  const { jsonPanel, helpPanel, onNavigate } = useLookerHelpers();
-
-  const hoveringSample = useRecoilValue(fos.hoveredSample);
-  const currentModalSampleId = useRecoilValue(fos.modalSampleId);
+  const { jsonPanel, helpPanel } = useLookerHelpers();
 
   const select = fos.useSelectSample();
 
@@ -179,13 +162,11 @@ const Modal = () => {
   fos.useEventHandler(document, "keyup", keysHandler);
 
   const isFullScreen = useRecoilValue(fos.fullscreen);
-  const sidebarwidth = useRecoilValue(fos.sidebarWidth(true));
-  const isSidebarVisible = useRecoilValue(fos.sidebarVisible(true));
 
   const screenParams = useMemo(() => {
     return isFullScreen
       ? { width: "100%", height: "100%" }
-      : { width: "95%", height: "90%", borderRadius: "3px" };
+      : { width: "95%", height: "calc(100% - 70px)", borderRadius: "8px" };
   }, [isFullScreen]);
 
   const activeLookerRef = useRef<fos.Lookers>();
@@ -231,12 +212,6 @@ const Modal = () => {
         <TooltipInfo />
         <ModalContainer style={{ ...screenParams }}>
           <OperatorPromptArea area={OPERATOR_PROMPT_AREAS.DRAWER_LEFT} />
-          <ModalNavigationContainer
-            $isSidebarVisible={isSidebarVisible}
-            $sidebarWidth={sidebarwidth}
-          >
-            <ModalNavigation onNavigate={onNavigate} />
-          </ModalNavigationContainer>
           <SpacesContainer>
             <ModalSpace />
           </SpacesContainer>
